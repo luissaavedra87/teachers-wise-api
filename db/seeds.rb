2a9e2c3a53d0eb5d33f64d47d5302fa23e47921f
Luis Saavedra <nriqu322@gmail.com>
2020-10-05 11:59:35 -0600

    teachers list seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Teacher.create!(
+  name: 'Carolina Galvez',
+  image: '/images/teacher1.jpg',
+  description: 'I have 4 years of experience and I\'m a especialist in teaching kids.'
+)
+
+Teacher.create!(
+  name: 'Kenneth Peterson',
+  image: '/images/teacher2.jpg',
+  description: 'I have 4 years of experience and I\'m a especialist in teaching kids.'
+)
+
+Teacher.create!(
+  name: 'Fatima Sani',
+  image: '/images/teacher3.jpg',
+  description: 'I have 4 years of experience and I\'m a especialist in teaching kids.'
+)
+
+Teacher.create!(
+  name: 'Van Nam Nguyen',
+  image: '/images/teacher4.jpg',
+  description: 'I have 4 years of experience and I\'m a especialist in teaching kids.'
+)
+
+Teacher.create!(
+  name: 'Karen Williams',
+  image: '/images/teacher5.jpg',
+  description: 'I have 4 years of experience and I\'m a especialist in teaching kids.'
+)
+
+Teacher.create!(
+  name: 'Charles Bernard',
+  image: '/images/teacher6.jpg',
+  description: 'I have 2 years of experience and I\'m a especialist in pronuntiation.'
+)
