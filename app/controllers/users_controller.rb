b819724a489e409526d993adde463aaa1be551d2
Luis Saavedra <nriqu322@gmail.com>
2020-10-06 16:15:24 -0600

    create users resources

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      user = user
+      token = JWT.encode({user_id: user_id}, secret)
+      render json: {user: user, token: token}
+    else
+      render json: {errors: user.errors.full_messages}
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_170639) do
+ActiveRecord::Schema.define(version: 2020_10_06_182158) do
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_10_05_170639) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
