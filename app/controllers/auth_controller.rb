46092bc45be687e43ac40123c07497c3e3209acf
Luis Saavedra <nriqu322@gmail.com>
2020-10-06 16:56:39 -0600

    add authorization controller

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def login
+    user = User.find_by(username: login_params[:username])
+    if user.authenticate(login.params[:password])
+      token = JWT.encode({user_id: user_id}, secret)
+      render json: {user: user, token: token}
+    end
+  end
+
+  def session
+    if request.headers['Authorization']
+      encoded_token = request.headers['Authorization'].split('')[1]
+      token = JWT.decode(encoded_token, secret)
+      user_id = token[0]['user_id']
+      user = User.find(user_id)
+      render json: user
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:username, :email, :password)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:username, :email, :password, :password_confirmation)
+    params.permit(:username, :email, :password)
   end
 end
