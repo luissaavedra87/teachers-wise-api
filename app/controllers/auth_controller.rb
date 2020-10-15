802423894bbcebe08c97e228bb2f6421739c6fea
Luis Saavedra <nriqu322@gmail.com>
2020-10-15 10:15:06 -0600

    fix user.id in controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,7 @@ class AuthController < ApplicationController
   def login
     user = User.find_by(username: login_params[:username])
     if user && user.authenticate(login.params[:password])
-      token = JWT.encode({user_id: user_id}, secret, 'HS256')
+      token = JWT.encode({user_id: user.id}, secret, 'HS256')
       render json: {user: user, token: token, sucess: "Welcome back, #{user.username}"}
     else
       render json: {errors: user.errors.full_messages}
