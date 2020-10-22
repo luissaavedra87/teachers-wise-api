d6273e25e1d7ed3fc229bddbb574406ac77ef6bf
Luis Saavedra <nriqu322@gmail.com>
2020-10-22 13:57:54 -0600

    fix login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.permit(:username, :email, :password, :password_confirmation)
   end
 end
