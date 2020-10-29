c3183bbc64d26303c65498a9698a5cac94560cf5
Luis Saavedra <nriqu322@gmail.com>
2020-10-28 19:39:38 -0600

    add appoinment relations to users and teachers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  has_many :appointments, dependent: :destroy
 end
