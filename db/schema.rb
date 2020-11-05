1aa59990d1141916f9e206ebabc4eef34ec16c2e
Luis Saavedra <nriqu322@gmail.com>
2020-11-05 00:31:30 -0600

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,34 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_007_011_939) do
-  create_table 'appoinments', force: :cascade do |t|
-    t.datetime 'schedule'
-    t.boolean 'status'
-    t.integer 'user_id', null: false
-    t.integer 'teacher_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['teacher_id'], name: 'index_appoinments_on_teacher_id'
-    t.index ['user_id'], name: 'index_appoinments_on_user_id'
+ActiveRecord::Schema.define(version: 2020_10_07_011939) do
+
+  create_table "appoinments", force: :cascade do |t|
+    t.datetime "schedule"
+    t.boolean "status"
+    t.integer "user_id", null: false
+    t.integer "teacher_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["teacher_id"], name: "index_appoinments_on_teacher_id"
+    t.index ["user_id"], name: "index_appoinments_on_user_id"
   end
 
-  create_table 'teachers', force: :cascade do |t|
-    t.string 'name'
-    t.string 'image'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'description'
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'appoinments', 'teachers'
-  add_foreign_key 'appoinments', 'users'
+  add_foreign_key "appoinments", "teachers"
+  add_foreign_key "appoinments", "users"
 end
